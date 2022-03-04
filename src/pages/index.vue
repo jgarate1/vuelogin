65e275ba1d4f1655b34c712c537d8d83add59fb6
jgarate1 <98298564+jgarate1@users.noreply.github.com>
2022-03-03 20:27:43 -0600

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import useAuth from "../composable/useAuth";
+const { isAuthenticated, user } = useAuth();
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-center justify-center space-y-12  min-h-screen-nonav"
+  >
+    <h2
+      v-if="!isAuthenticated"
+      class="text-6xl tracking-tighter font-extralight"
+    >
+      Welcome to my website!
+    </h2>
+    <h2 v-else class="text-6xl tracking-tighter font-extralight">
+      Welcome master {{ user }}.
+    </h2>
+    <img src="../assets/bgindex.png" alt="Hello BG" />
+  </div>
+</template>
---
+++ src/pages/secret.vue
@@ -0,0 +1,5 @@
+<template>
+  <p>
+   This is a secret message that you can only see if you login. Congrats, you win $100,000.
+  </p>
+</template>
